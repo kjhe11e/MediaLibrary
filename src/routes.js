57f06b3007a5fbe1a551d0c90dfb246396bd73a7
Kyle Helle <kjhelle4@gmail.com>
2017-02-14 14:14:07 -0800

    Add library route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import HomePage from './components/HomePage';
+import MediaGalleryPage from './containers/MediaGalleryPage';
 
 // map components to different routes.
 // parent component wraps other components and servers as entrance to other React components.
@@ -9,5 +10,6 @@ import HomePage from './components/HomePage';
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={HomePage} />
+		<Route path="libary" component={MediaGalleryPage} />
 	</Route>
 );
